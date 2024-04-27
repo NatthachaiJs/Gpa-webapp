6f5e3d12cf844914515113b40c052e11d13b42d5
Natthachai Jansri <63160004@go.buu.ac.th>
2024-04-27 14:14:57 +0700

    Main  Screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddScreenComponent } from './add-screen/add-screen.component';
 import { ScoreScreenComponent } from './score-screen/score-screen.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddScreenComponent,
-    ScoreScreenComponent
+    ScoreScreenComponent,
+    MainScreenComponent
   ],
   imports: [
     BrowserModule,
